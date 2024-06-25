213f3a10ea8f9a2cd3dfcc97ab451657b9072083
David111922 <salve62@yahoo.com>
2024-06-24 23:33:30 -0700

    added store listings

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Background() {
   return (
-    <Card style={{ width: '160rem', backgroundColor: '#deecd6', margin: 'auto' }}>
-      <Card.Body className="text-center">
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text style={{fontSize: '40px' , fontWeight: 'bold'}}>
-          Order Groceries for delivery today.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <div style={{ alignSelf: 'flex-end', marginRight: '10px' }}>
-        <Card.Img 
-          src="aguacate.webp" 
-          style={{ width: '200px', height: '200px' }} 
-        />
-      </div>
-    </Card>
+    <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh', backgroundColor: '#deecd6' }}>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Card style={{ width: '160rem', backgroundColor: '#deecd6', margin: 'auto' }}>
+            <Card.Body className="text-center">
+              <Card.Text style={{ fontSize: '40px', fontWeight: 'bold' }}>
+                Order Groceries for <br /> delivery today.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <div style={{ alignSelf: 'flex-end', marginRight: '10px' }}>
+              <Card.Img 
+                src="aguacate.webp" 
+                style={{ width: '200px', height: '200px' }} 
+              />
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/Components/StoreListing.js
+++ src/Components/StoreListing.js
@@ -1,44 +1,179 @@
-import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function StoreListing() {
   return (
-    <>
-      <h2>Browse stores in Houston</h2>
-      <Container style={{ marginTop: "40px", textAlign: "center" }}>
-        <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly" }}>
-          <Row>
-            <Col xs="auto">
-              <Image src="Vons.webp" roundedCircle />
-            </Col>
-            <Col>
+    <div>
+      <Container style={{ marginTop: "40px" }}>
+        <Row
+          style={{
+            borderBottom: "1px solid #E8E9EB",
+            borderBottom: "1px solid #E8E9EB",
+            display: "flex",
+            justifyContent: "space-evenly",
+            padding: "0 20px 20px 20px",
+          }}
+        >
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="Vons.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
               <h5 style={{ fontWeight: "bold" }}>Vons</h5>
               <p>Delivery * Pickup</p>
-            </Col>
-          </Row>
-          <Col sm="1"></Col>
-          <Row>
-            <Col xs="auto">
-              <Image src="Sprouts.webp" roundedCircle />
-            </Col>
-            <Col>
-              <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+            </div>
+          </Col>
+
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="Sprouts.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
+              <h5 style={{ fontWeight: "bold" }}>Sprouts</h5>
               <p>Delivery * Pickup</p>
-            </Col>
-          </Row>
-          <Col sm="1"></Col>
-          <Row>
-            <Col xs="auto">
-              <Image src="[IMG SRC]" roundedCircle />
-            </Col>
-            <Col md="6">
-              <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
-              <p>Delivery</p>
-            </Col>
-          </Row>
+            </div>
+          </Col>
+
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="Costco.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
+              <h5 style={{ fontWeight: "bold" }}>Costco</h5>
+              <p>Delivery * Pickup</p>
+            </div>
+          </Col>
+        </Row>
+
+        <br/>
+
+        <Row
+          style={{
+            borderBottom: "1px solid #E8E9EB",
+            borderBottom: "1px solid #E8E9EB",
+            display: "flex",
+            justifyContent: "space-evenly",
+            padding: "0 20px 20px 20px",
+          }}
+        >
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="Albertsons.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
+              <h5 style={{ fontWeight: "bold" }}>Albertsons</h5>
+              <p>Delivery * Pickup</p>
+            </div>
+          </Col>
+
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="Walgreens.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
+              <h5 style={{ fontWeight: "bold" }}>Walgreens</h5>
+              <p>Delivery * Pickup</p>
+            </div>
+          </Col>
+
+          <Col
+            xs="3"
+            style={{
+            
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "20px",
+              height: "6rem",
+            }}
+          >
+            <Image
+              src="La Bonita.webp"
+              style={{
+                border: "1px solid #ececee",
+                borderRadius: "50%",
+                width: "25%",
+              }}
+            />
+            <div style={{  width: "80%", display: 'flex', textAlign: 'start', flexFlow: 'column', marginLeft: '20px' }}>
+              <h5 style={{ fontWeight: "bold" }}>La Bonita</h5>
+              <p>Delivery * Pickup</p>
+            </div>
+          </Col>
         </Row>
       </Container>
-    </>
+    </div>
   );
 }
 
